a5b0f27c2a8eb376b8acd70bd1f1ab70312fa283
Griffin Zavadil <griffinz@rams.colostate.edu>
2022-03-07 19:32:47 -0700

    Make client send X messages per second

--- src/cs455/scaling/Client.java
+++ src/cs455/scaling/Client.java
@@ -48,36 +48,41 @@ public class Client {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		HashMessage nextMessage = new HashMessage();
-		String messageToSend_Unhashed = nextMessage.getByteArray().toString();
-		String messageToSend_Hashed = nextMessage.getHashedString();
-		//System.out.println("Raw message: " + new String(messageToSend_Unhashed) + ".getBytes() = " + new String(messageToSend_Unhashed).getBytes());
-System.out.println("Going to write: " + new String(messageToSend_Unhashed) + " to the server.");
-//		System.out.println("The server should respond with: " + new String(messageToSend_Hashed).trim());
-	
-		//buffer = ByteBuffer.wrap("Please send this back to me".getBytes());
-		buffer = ByteBuffer.wrap(messageToSend_Unhashed.getBytes());
-		String response = null;
-		//System.out.println("Going to write (as byte[]) : " + buffer.array());
-		
-		try{
-			client.write(buffer);
-			buffer.clear();
-			client.read(buffer);
-			response = new String(buffer.array()).trim();
-			System.out.println("Server responded with: " + response);
-			boolean successfulHash = messageToSend_Hashed == response;
-			System.out.println("Did it hash correctly? -> " + successfulHash);
-			buffer.clear();
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
 
 		PrintStatsThread pst = new PrintStatsThread(this);
 		pst.start();
 
+		while (true) {
+		
+			HashMessage nextMessage = new HashMessage();
+			String messageToSend_Unhashed = nextMessage.getByteArray().toString();
+			String messageToSend_Hashed = nextMessage.getHashedString();
+			//System.out.println("Raw message: " + new String(messageToSend_Unhashed) + ".getBytes() = " + new String(messageToSend_Unhashed).getBytes());
+			System.out.println("Going to write: " + new String(messageToSend_Unhashed) + " to the server.");
+			//System.out.println("The server should respond with: " + new String(messageToSend_Hashed).trim());
+		
+			//buffer = ByteBuffer.wrap("Please send this back to me".getBytes());
+			buffer = ByteBuffer.wrap(messageToSend_Unhashed.getBytes());
+			String response = null;
+			//System.out.println("Going to write (as byte[]) : " + buffer.array());
+
+			try{
+				client.write(buffer);
+				incrementTotalSent();
+				buffer.clear();
+				client.read(buffer);
+				incrementTotalReceived();
+				response = new String(buffer.array()).trim();
+				System.out.println("Server responded with: " + response);
+				boolean successfulHash = messageToSend_Hashed == response;
+				System.out.println("Did it hash correctly? -> " + successfulHash);
+				buffer.clear();
+				Thread.sleep(1000 / messageRate);
+			}
+			catch (IOException | InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public synchronized void incrementTotalSent(){
@@ -105,12 +110,12 @@ System.out.println("Going to write: " + new String(messageToSend_Unhashed) + " t
 			int serverPort = Integer.parseInt(args[1]);
 			// Third argument is message-rate
 			int messageRate = Integer.parseInt(args[2]);
-			System.out.println("Starting client w/ serverHost: " + serverHost + ", serverPort: " + serverPort + ", messageRate: once every " + messageRate + " seconds.");
+			System.out.println("Starting client w/ serverHost: " + serverHost + ", serverPort: " + serverPort + ", messageRate: " + messageRate + " messages per second.");
 			Client clientObj = new Client(serverHost, serverPort, messageRate);
 		}
 
 		else {
-			System.out.println("No program arguments specified, please specify: (1) serverHost, (2) serverPort, and (3) messageRate (once every X seconds)");
+			System.out.println("No program arguments specified, please specify: (1) serverHost, (2) serverPort, and (3) messageRate (X messages per second)");
 		}
 		return;
 	}
