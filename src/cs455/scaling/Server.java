184932d1562ad7e4906f98088fa60bb3bcd7a422
Griffin Zavadil <griffinz@rams.colostate.edu>
2022-03-07 18:48:25 -0700

    Add server portnum argument

--- src/cs455/scaling/Server.java
+++ src/cs455/scaling/Server.java
@@ -15,22 +15,24 @@ public class Server {
 
 	public static void main(String[] args) throws IOException {
 	
-		if (args.length == 3){
-			// First argument is thread-pool-size
-			int threadPoolSize = Integer.parseInt(args[0]);
-			// Second argument is batch-size
-			int batchSize = Integer.parseInt(args[1]);
-			// Third argument is batch-time		
-			int batchTime = Integer.parseInt(args[2]);
-			System.out.println("Starting server w/ threadPoolSize: " + threadPoolSize + ", batchSize: " + batchSize + ", batchTime: " + batchTime + " seconds.");
-			ThreadPoolManager tpm = new ThreadPoolManager(threadPoolSize);
+		if (args.length == 4){
+			// First argument is portnum
+			int portnum = Integer.parseInt(args[0]);
+			// Second argument is thread-pool-size
+			int threadPoolSize = Integer.parseInt(args[1]);
+			// Third argument is batch-size
+			int batchSize = Integer.parseInt(args[2]);
+			// Fourth argument is batch-time		
+			int batchTime = Integer.parseInt(args[3]);
+			System.out.println("Starting server w/ portnum: " + portnum + ", threadPoolSize: " + threadPoolSize + ", batchSize: " + batchSize + ", batchTime: " + batchTime + " seconds.");
+			ThreadPoolManager tpm = new ThreadPoolManager(portnum, threadPoolSize);
 			tpm.run();
 
 
 		}
 
 		else{
-			System.out.println("No program arguments specified, please specify: (1) threadPoolSize, (2) batchSize, (3) batchTime (in seconds).");
+			System.out.println("No program arguments specified, please specify: (1) portnum, (2) threadPoolSize, (3) batchSize, (4) batchTime (in seconds).");
 		}
 		// Potential Thread Pool Manager
 
